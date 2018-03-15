3aab51345c6a0e8f417cd6788d968ce44f7352dd
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-15 13:21:49 +0300

    middleware coments

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -10,7 +10,7 @@ const routes = (app) => {
   app.route('/contact')    
     .get((req, res, next) => {    
       // here I place a middleware to intercept request data for further
-      // use, e.g. collecting results for evaluation tasks 
+      // use, e.g. collecting results for tasks evaluation 
       console.log(`Request from: ${req.originalUrl}`)
       console.log(`Request type: ${req.method}`)
       next();
